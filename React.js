1d867889e75084463595ce9cc01537cae8d98f1d
DHIRAJ KUMAR <110246188+dhiraj7kr@users.noreply.github.com>
2025-01-22 10:46:25 +0530

    Create  React.js

---
+++ React.js
@@ -0,0 +1,28 @@
+npm install axios
+
+
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+const App = () => {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:5000')
+      .then(response => {
+        setMessage(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <View>
+      <Text>{message}</Text>
+    </View>
+  );
+};
+
+export default App;
